dad1148ce2646dadd20791ceff637e09595857ed
Vitor Paulo <vitorblog12@gmail.com>
2026-09-02 22:34:26 -0300

    fix: use empty template for silent /spyglass command

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export const AgentSpyglass: Plugin = async (plugin: PluginInput) => {
 	return {
 		config: async (ocConfig) => {
 			ocConfig.command ??= {}
-			ocConfig.command['spyglass'] = {template: 'Do not explain, acknowledge, or comment. Output nothing at all.', description: 'Toggle SpyGlass view.'}
+			ocConfig.command['spyglass'] = {template: '', description: 'Toggle SpyGlass view.'}
 		},
 
 		dispose: async () => {
